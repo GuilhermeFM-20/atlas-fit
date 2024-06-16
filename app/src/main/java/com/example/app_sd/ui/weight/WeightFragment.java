99874c7be0d8a08a7a1e2fe4a6c4b96fd075a76b
Juan Pedro <jpss332211@gmail.com>
2024-06-16 20:22:50 -0300

    feat: Implementação da tela de cadastro de peso

--- app/src/main/java/com/example/app_sd/ui/weight/WeightFragment.java
+++ app/src/main/java/com/example/app_sd/ui/weight/WeightFragment.java
@@ -1,4 +1,4 @@
-package com.example.app_sd.ui.perfil;
+package com.example.app_sd.ui.weight;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -15,10 +15,13 @@ import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.example.app_sd.R;
-import com.example.app_sd.databinding.FragmentPerfilBinding;
+import com.example.app_sd.databinding.FragmentWeightBinding;
 import com.example.app_sd.service.ApiService;
+import com.example.app_sd.ui.weight.WeightViewModel;
+import com.google.android.material.button.MaterialButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,9 +29,9 @@ import org.json.JSONObject;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class PerfilFragment extends Fragment {
+public class WeightFragment extends Fragment {
 
-    private FragmentPerfilBinding binding;
+    private FragmentWeightBinding binding;
     protected String prefix = "/users/";
 
     @Override
@@ -37,13 +40,22 @@ public class PerfilFragment extends Fragment {
 
 
 
-        FragmentPerfilBinding binding = FragmentPerfilBinding.inflate(inflater, container, false);
+        FragmentWeightBinding binding = FragmentWeightBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        MaterialButton insertButton = root.findViewById(R.id.button2);
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                atualizarButton(v);
+            }
+        });
 
-        PerfilViewModel homeViewModel = new ViewModelProvider(this).get(PerfilViewModel.class);
 
-        Spinner spinner = binding.combo;
+
+        WeightViewModel homeViewModel = new ViewModelProvider(this).get(WeightViewModel.class);
+
+        //Spinner spinner = binding.combo;
 
 
         // Obtendo a Intent da Activity associada ao Fragment
@@ -59,21 +71,7 @@ public class PerfilFragment extends Fragment {
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = parent.getItemAtPosition(position).toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Nada a fazer
-            }
-        });
-
-
+        //spinner.setAdapter(adapter);
 
 
         ExecutorService executorService = Executors.newSingleThreadExecutor();
@@ -113,6 +111,9 @@ public class PerfilFragment extends Fragment {
         return root;
     }
 
+    public void atualizarButton(View view){
+        Navigation.findNavController(view).navigate(R.id.nav_insert_activities);
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
