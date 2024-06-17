9af37368620bc8a4b0b3ec595b04d0be43827027
GuilhermeFM-20 <guilherme_freire_melo@outlook.com>
2024-06-17 04:41:01 -0300

    feat: Ajuste geral do merge

--- app/src/main/java/com/example/app_sd/ui/activities/ActivitiesInsertFragment.java
+++ app/src/main/java/com/example/app_sd/ui/activities/ActivitiesInsertFragment.java
@@ -3,23 +3,30 @@ package com.example.app_sd.ui.activities;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.example.app_sd.R;
 import com.example.app_sd.databinding.FragmentActivitiesBinding;
 import com.example.app_sd.databinding.FragmentActivitiesInsertBinding;
+import com.example.app_sd.service.ApiService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ActivitiesInsertFragment extends Fragment {
 
@@ -41,7 +48,7 @@ public class ActivitiesInsertFragment extends Fragment {
         ActivitiesViewModel homeViewModel = new ViewModelProvider(this).get(ActivitiesViewModel.class);
 
 
-        Spinner spinner = binding.tipo;
+        Spinner spinner = binding.inputType;
 
         String[] items = {"Selecione uma opção...","Aeróbica", "Musculação", "Flexibilidade", "Equilíbrio"};
 
@@ -66,14 +73,14 @@ public class ActivitiesInsertFragment extends Fragment {
             }
         });
 
-        Spinner spinner2 = binding.intensidade;
+        Spinner spinner2 = binding.inputIntensity;
 
         String[] items2 = {"Selecione uma opção...","Baixa", "Moderada", "Alta"};
 
 
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, items2);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner2.setAdapter(adapter2);
 
