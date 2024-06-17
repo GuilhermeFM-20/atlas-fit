9cae7dcbe4fd0e3cd928ccdc9ece6782748ef1be
GuilhermeFM-20 <guilherme_freire_melo@outlook.com>
2024-06-17 05:06:43 -0300

    feat: Ajustes geral das telas que precisavam de correções

--- app/src/main/java/com/example/app_sd/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/app_sd/ui/login/LoginFragment.java
@@ -90,7 +90,7 @@ public class LoginFragment extends Fragment {
                             }
 
 
-                            //if (status == "true") {
+                            if (status == "true") {
                                 // Usar NavController para navegação
 
                                 String id = null;
@@ -106,9 +106,9 @@ public class LoginFragment extends Fragment {
                                 editor.putInt("USER_ID", Integer.parseInt(id));
                                 editor.apply();
                                 startActivity(intent);
-                            /*} else {
+                            } else {
                                 getActivity().runOnUiThread(() -> Toast.makeText(getActivity(), "Usuário ou senha inválido.", Toast.LENGTH_SHORT).show());
-                            }*/
+                            }
 
 
                         });
